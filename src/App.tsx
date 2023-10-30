7f35874d2dfcb4e11bb162247a181eba8ae8026a
Kasyanovskaya Kristina <kristinochkakv@gmail.com>
2023-10-30 20:20:16 +0300

    feat: start week2 and change App.tsx for functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { Component } from "react";
 import "./App.css";
 import Home from "./components/Home/Home";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 import ErrorBoundaryButton from "./components/ErrorBoundary/ErrorBoundaryButton";
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
-        <ErrorBoundary>
-          <Home />
-          <ErrorBoundaryButton/>
-        </ErrorBoundary>
-      </>
-    );
-  }
+export function App() {
+  return (
+    <>
+      <ErrorBoundary>
+        <Home />
+        <ErrorBoundaryButton />
+      </ErrorBoundary>
+    </>
+  );
 }
